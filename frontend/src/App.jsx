8a79930f9c1bbc8279c8681996fc10986a22b50d
KaviduIsura <kaviduisura4567@gmail.com>
2025-12-29 16:21:02 +0530

    init frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="bg-red-600 min-h-screen flex items-center justify-center">
+      <h1 className="text-white text-3xl font-bold">
+        Laptop Price Predictor
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
